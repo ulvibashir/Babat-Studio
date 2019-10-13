7b7d0d6a830f1d72d63e7d0ef23baa99925dc4ef
Nihad Alekberzade <elekberzadeh333@gmail.com>
2019-10-13 23:30:45 +0400

    useless event deleted, connection established

--- BabatStudio/Presenters/NewProjectPresenter.cs
+++ BabatStudio/Presenters/NewProjectPresenter.cs
@@ -13,60 +13,28 @@ namespace BabatStudio.Presenters
 {
     class NewProjectPresenter 
     {
-        public INew_Project _new_Project { get; set; }
+        public INewProjectView _newProjectForm { get; set; }
 
-        public string ProjectPath { get; set; }
-        public bool CreateSubdirCheck { get; set; }
-        public string ProjectName { get; set; }
-
-        public NewProjectPresenter(INew_Project new_Project)
+        public NewProjectPresenter(INewProjectView new_Project)
         {
-            _new_Project = new_Project;
-            _new_Project.CancelEvent += CancelPR;
-            _new_Project.OkEvent += OkPR;
-            _new_Project.CreateSubdirEvent += CreateSubdirPR;
-            _new_Project.ChooseFolderEvent += ChooseFolderPR;
-            _new_Project.TextChangedEvent += TextChangedPR;
+            _newProjectForm = new_Project;
+            _newProjectForm.ChooseFolderEvent += ChooseFolderPR;
+           
         }
 
         public void ChooseFolderPR(object txtFolder, EventArgs e)
         {
-
             using (var fbd = new FolderBrowserDialog())
             {
                 DialogResult result = fbd.ShowDialog();
 
                 if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
                 {
-                    ProjectPath = fbd.SelectedPath;
-                    (txtFolder as TextBox).Text = ProjectPath;
+                    _newProjectForm.ProjectPath = fbd.SelectedPath;
+                    (txtFolder as TextBox).Text = _newProjectForm.ProjectPath; 
                 }
             }
         }
 
-        public void CreateSubdirPR(object sender, EventArgs e)
-        {
-            if ((sender as CheckBox).Checked == true)
-                CreateSubdirCheck = true;
-            else
-                CreateSubdirCheck = false;
-        }
-
-        public void CancelPR(object sender, EventArgs e)
-        {
-           
-        }
-
-        public void OkPR(object sender, EventArgs e)
-        {
-            
-        }
-
-        public void TextChangedPR(object sender, EventArgs e)
-        {
-            ProjectName = (sender as TextBox).Text;
-        }
-
-        
     }
 }
